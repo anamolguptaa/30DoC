ea3120e20afa4b0ca43c0ec3867b43009c5866ee
Anmol Gupta <91716912+anamolguptaa@users.noreply.github.com>
2021-12-12 09:35:26 +0530

    Update sum.c

--- mydirectory/sum.c
+++ mydirectory/sum.c
@@ -2,11 +2,11 @@
 int main(){
 int a,b,sum;
 printf("Enter the first number:\n");
-scanf("%d",a);
+scanf("%d",&a);
 printf("Enter the second number:\n");
-scanf("%d",b);
+scanf("%d",&b);
 sum = a+b;
-printf("The sum of the two numbers is:\n",sum);
+printf("The sum of the two numbers is: %d \n",sum);
 return 0;
 }
 
